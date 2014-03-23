2b9fb79017e03e65c83f442b5be17f44b453b4d0
Maurizio Taverna <tavernamaurizio@gmail.com>
2014-03-24 00:33:19 +0100

    Initial Import

---
+++ www/js/application.js
@@ -0,0 +1,7 @@
+angular.module('moocs4u', ["ngRoute","mobile-angular-ui","mobile-angular-ui.touch","mobile-angular-ui.scrollable"])
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            // ...
+        });
+        // ...
+    });
