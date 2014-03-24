7b7146f9cd4d0e5319e148777571feda65174f28
Maurizio Taverna <tavernamaurizio@gmail.com>
2014-03-24 01:59:33 +0100

    Initial Import

--- www/js/application.js
+++ www/js/application.js
@@ -1,8 +1,11 @@
 angular.module('moocs4u', ["ngRoute","mobile-angular-ui","mobile-angular-ui.touch","mobile-angular-ui.scrollable"])
     .config(function($routeProvider) {
-        $routeProvider.when('/about', {
-            templateUrl: "partials/about.tpl.html"}
-        );
+        $routeProvider
+            .when('/about', {
+                templateUrl: "partials/about.tpl.html"}
+            ).otherwise({
+                redirectTo: '/about'
+            });
         // ...
     }).controller('MainController', function($rootScope, $scope){
 
