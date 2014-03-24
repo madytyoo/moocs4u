a2b0077ae270979d93d4a88a970c4b646782f200
Maurizio Taverna <tavernamaurizio@gmail.com>
2014-03-24 14:22:38 +0100

    Initial Import

--- www/js/application.js
+++ www/js/application.js
@@ -2,12 +2,26 @@ angular.module('moocs4u', ["ngRoute","mobile-angular-ui","mobile-angular-ui.touc
     .config(function($routeProvider) {
         $routeProvider
             .when('/about', {
-                templateUrl: "partials/about.tpl.html"}
+                templateUrl: "partials/about.tpl.html"})
+            .when('/coursera', {
+                templateUrl: "partials/coursera.tpl.html"}
             ).otherwise({
                 redirectTo: '/about'
             });
         // ...
-    }).controller('MainController', function($rootScope, $scope){
+    }).config(function ( $httpProvider) {
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }).controller('MainController', function($rootScope, $scope,$http){
+        $scope.platforms= {};
+        $http.get('http://test-crawler-moocs4u.appspot.com/api/1.0/platforms')
+            .success(function(data){
+                $scope.platforms = data;
+                console.log( "yay" );
+            })
+            .error(function(d){ console.log( "nope" ); });
+
+
+
 
         $rootScope.$on("$routeChangeStart", function(){
             $rootScope.loading = true;
