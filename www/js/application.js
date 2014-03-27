5ef0adfc4be3bd6ae35e45bb42f959cc682b5957
Maurizio Taverna <tavernamaurizio@gmail.com>
2014-03-27 17:16:21 +0100

    Initial Import

--- www/js/application.js
+++ www/js/application.js
@@ -1,9 +1,10 @@
 var host = "http://test-crawler-moocs4u.appspot.com"
-//var host = "http://127.0.0.1:8081";
+//var host = "http://192.168.1.5:8081";
 angular.module('moocs4u', ["ngRoute","mobile-angular-ui","mobile-angular-ui.touch","mobile-angular-ui.scrollable"])
     .config(function($routeProvider) {
         $routeProvider
             .when('/about', {
+                controller: 'MainCtrl',
                 templateUrl: "partials/about.tpl.html"})
             .when('/platform/:platformID', {
                 controller: 'PlatformCtrl',
@@ -11,42 +12,65 @@ angular.module('moocs4u', ["ngRoute","mobile-angular-ui","mobile-angular-ui.touc
             ).when('/category/:categoryID', {
                 controller: 'CategoryCtrl',
                 templateUrl: "partials/category.tpl.html"}
+            ).when('/course/:courseID', {
+                controller: 'CourseCtrl',
+                templateUrl: "partials/course.tpl.html"}
             ).otherwise({
                 redirectTo: '/about'
             });
         // ...
     }).config(function ( $httpProvider) {
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }).controller('MainCtrl', function($rootScope, $scope,$http){
+    }).controller('MainCtrl', function($rootScope, $scope,$http,$log){
         $scope.platforms= {};
+
         $http.get( host + '/api/1.0/platforms')
             .success(function(data){
+                $log.info("")
                 $scope.platforms = data;
-                console.log( "yay" );
+                $rootScope.loading = false;
             })
-            .error(function(d){ console.log( "nope" ); });
+            .error(function(d){
+                $rootScope.loading = false;
+            });
 
-        $rootScope.$on("$routeChangeStart", function(){
-            $rootScope.loading = true;
+            $rootScope.$on("$routeChangeStart", function(){
+                $rootScope.loading = true;
+            });
+            $rootScope.$on("$routeChangeEnd", function(){
+                $rootScope.loading = false;
+            });
+
+        }).controller('PlatformCtrl', function($rootScope, $scope,$http,$routeParams){
+
+            $rootScope.$on("$routeChangeStart", function(){
+                $rootScope.loading = true;
+            });
+
+            $scope.platform = {};
+            if( $routeParams.platformID != undefined ) {
+                $http.get(host + '/api/1.0/platform?id=' + $routeParams.platformID ).success(function (data) {
+                    $scope.platform = data;
+                    $rootScope.loading = false;
+                })
+            }
+
+        }).controller('CategoryCtrl', function($rootScope, $scope,$http,$routeParams){
+            $scope.category = {};
+            if( $routeParams.categoryID != undefined ) {
+                $http.get(host + '/api/1.0/category?id=' + $routeParams.categoryID ).success(function (data) {
+                    $scope.category = data;
+                    $rootScope.loading = false;
+                })
+            }
+
+        }).controller('CourseCtrl', function($rootScope, $scope,$http,$routeParams){
+            $scope.course = {};
+            if( $routeParams.courseID != undefined ) {
+                $http.get(host + '/api/1.0/course?id=' + $routeParams.courseID ).success(function (data) {
+                    $scope.course = data;
+                    $rootScope.loading = false;
+                })
+            }
+
         });
-
-        $rootScope.$on("$routeChangeSuccess", function(){
-            $rootScope.loading = false;
-        });
-    }).controller('PlatformCtrl', function($rootScope, $scope,$http,$routeParams){
-        $scope.platform = {};
-        if( $routeParams.platformID != undefined ) {
-            $http.get(host + '/api/1.0/platform?id=' + $routeParams.platformID ).success(function (data) {
-                $scope.platform = data;
-            })
-        }
-
-    }).controller('CategoryCtrl', function($rootScope, $scope,$http,$routeParams){
-        $scope.category = {};
-        if( $routeParams.categoryID != undefined ) {
-            $http.get(host + '/api/1.0/category?id=' + $routeParams.categoryID ).success(function (data) {
-                $scope.category = data;
-            })
-        }
-
-    });
